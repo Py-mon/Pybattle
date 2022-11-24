bc72563a11976833a654a68a9ca4b7464198f113
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2022-11-24 15:27:08 -0600

    Delete src directory

--- src/help.py
+++
@@ -1,8 +0,0 @@
-from typing import Callable, Any
-from inspect import getfullargspec
-
-
-# Prints info of any function in format: function(arg1, arg2, arg3, ...)
-def function_info(func: Callable[[..., Any], Any]):
-    args = ', '.join(getfullargspec(func).args)
-    return f'{func.__name__}({args})'
