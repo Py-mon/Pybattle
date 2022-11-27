ac1a6a9661a31969e2fe6d505b8da5d5f497c5a1
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2022-11-26 19:18:34 -0600

    Added Name at top

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from os import system
-from time import sleep
 
 from src.window.frame import Frame, Window
 
-system('cls') # Change to "clear" if you are not on windows
-
+system('cls')  # Change to "clear" if you are not on windows
 
 map = Window('''\
                    ||||       
@@ -17,6 +15,6 @@ map = Window('''\
 ╰│╯   ╶─╯           ╭─────┬─╮ 
                     │░░░░░│▓│ 
                     ╰─────┴─╯ 
-''')
+''', name='BEDROOM')
 
 map.add_frame(Frame(size=(2, 9)), (4, 0))
--- src/window/frame.py
+++ src/window/frame.py
@@ -1,6 +1,8 @@
+"""A border around some contents."""
+
 from typing import Optional
 
-from src.log import logger
+from src.log import Logger
 from src.types_ import CoordReference, SizeReference
 from src.window.coord import Coord, CoordList
 from src.window.matrix import Matrix
@@ -8,19 +10,24 @@ from src.window.size import Size
 
 
 class Frame:
-    """Get box chars here: https://en.wikipedia.org/wiki/Box-drawing_character"""
+    """A border around some contents.
+
+    Get box chars here: https://en.wikipedia.org/wiki/Box-drawing_character"""
 
     def __init__(
         self,
-        contents: Optional[str] = None,
-        size: SizeReference = ...
+        contents: Optional[str] | Matrix = None,
+        size: SizeReference = ...,
+        name: Optional[str] = None
     ) -> None:
         self.contents = contents
+        self.name = name
 
         if self.contents is not None:
             if size is not ...:
-                logger.info(f'Unused Frame size.')
-            self.contents = Matrix(contents)
+                Logger.info(f'Unused Frame size.', True)
+            if isinstance(contents, str):
+                self.contents = Matrix(contents)
             self.size = self.contents.size + 1
             self.size.height += 1  # Cuts off the button without
         elif size is not ...:
@@ -58,12 +65,15 @@ class Frame:
         return self.height - 1
 
     def _update_frame(self) -> None:
-        frame = f'╭{"─" * (self.width - 2)}╮\n'
+        if self.name is None:
+            frame = f'╭{"─" * (self.width - 2)}╮\n'
+        else:
+            frame = f'╭─ {self.name} {"─" * (self.width - 5 - len(self.name))}╮\n'
         for i in range(self.height - 2):
             if self.contents is None:
                 frame += f'│{" " * (self.width - 2)}│\n'
             else:
-                x = '\n' # Doesn't allow "\" in f-strings
+                x = '\n'  # Doesn't allow "\" in f-strings
                 frame += f'│{"".join(self.contents[i]).rstrip(x)}│\n'
 
         frame += f'╰{"─" * (self.width - 2)}╯\n'
@@ -167,8 +177,14 @@ class Frame:
                 if self.matrix[y + pos.y, x + pos.x] == ' ':
                     self.matrix[y + pos.y, x + pos.x] = frame_slice[y, x]
 
+        # Overlaps Name
+        if self.name is not None:
+            for i, char in enumerate(self.name + ' '):
+                self.matrix[0, i + 3] = char
+
         print(Matrix.convert_array(self.matrix))
 
 
 class Window(Frame):
+    """The main border screen."""
     pass
