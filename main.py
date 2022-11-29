7e294adfe5bb2ed478c229a7f7e715595cbd7d2d
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2022-11-28 20:50:25 -0600

    Attempted Color

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+# map = Window('''\
+#                    ||||
+#                    ||||
+#                      ─┬─┬─┬─┬─
+
+
+# ╭│╮   ╶─╮
+# │││    ░│
+# ╰│╯   ╶─╯           ╭─────┬─╮
+#                     │░░░░░│▓│
+#                     ╰─────┴─╯
+# ''', name='BEDROOM')
+
+# map.add_frame(Frame(size=(2, 9)), (4, 0))
+
 from os import system
 
-from src.window.frame import Frame, Window
+from src.window.color import Color
+from src.window.matrix import Matrix
+from src.window.screen import Screen
 
-system('cls')  # Change to "clear" if you are not on windows
+Screen.clear()  # doesn't work
 
-map = Window('''\
-                   ||||       
-                   ||||       
-                     ─┬─┬─┬─┬─
-                              
-                              
-╭│╮   ╶─╮                     
-│││    ░│                     
-╰│╯   ╶─╯           ╭─────┬─╮ 
-                    │░░░░░│▓│ 
-                    ╰─────┴─╯ 
-''', name='BEDROOM')
 
-map.add_frame(Frame(size=(2, 9)), (4, 0))
+system('cls')
+
+
+test_str = Matrix(f'''
+╭───────────╮
+│╭─╮        │
+││{Color.RED}a{Color.DEFAULT}│        │
+│╰─╯        │
+│   ╭─╮     │
+│   │b│     │
+│   ╰─╯     │
+│           │
+│           │
+│        ╭─╮│
+│        │c││
+│        ╰─╯│
+╰───────────╯
+''')
+
+print(test_str)
--- src/window/matrix.py
+++ src/window/matrix.py
@@ -1,22 +1,47 @@
 """Converts a str into a Matrix so it can be easily edited and then returned back to a str."""
 
-from numpy import row_stack, array
-from .size import Size
+from re import compile, sub
+
+from numpy import array, row_stack
+
+from src.window.size import Size
 
 
 class Matrix:
     """Converts a str into a Matrix so it can be easily edited and then returned back to a str."""
-    
+
     @staticmethod
     def convert_array(array_) -> str:
         return "".join([char for row in array_ for char in row])
-        
+
     def __init__(self, str_: str) -> None:
-        if str_[-1] != '\n':
-            str_ += '\n'
         self._str = str_
         
+        reg = compile(r'\xb1\[((?:\d|;)*)([a-zA-Z])')
+        self._str = sub(reg, '', self._str) # Deletes found matches
+        
+        print(self._str)
+        
+        # TODO: Fix (colors is always {})
+        colors: dict[str, tuple[int, int]] = {}
+        for match in reg.finditer(self._str): # Iterates through found matches
+            escape_code = match.group() # Matched sequence
+            y = len(self._str) // match.start()
+            x = len(self._str) % match.start()
+            colors[escape_code] = (x, y)
+        
+        rows = self._str.split('\n')
+        self._str = ''
+        for row in rows:
+            if row != '':
+                self._str += row + '\n'
+        
         self._matrix = row_stack([array(list(row)) for row in self._str.splitlines(True)])
+
+        for color, location in colors.items():
+            self._matrix[location] = color + self._matrix[location]
+        
+        print(self._matrix)
 
     def __len__(self) -> int:
         return len(self._matrix)
