96d18f996a0802e9582019f4826b296eac9046f8
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2022-12-01 20:07:05 -0600

    More Types

--- src/types_.py
+++ src/types_.py
@@ -3,25 +3,31 @@
 from typing import TYPE_CHECKING, TypeAlias, Union
 
 if TYPE_CHECKING:
-    from creatures.attributes.ability import Ability as Ability_
-    from creatures.attributes.element import Element
-    from creatures.attributes.item import Item as Item_
-    from creatures.attributes.move import Move as Move_
-    from creatures.attributes.reinforcements import Armor as Armor_
-    from creatures.attributes.reinforcements import Weapon as Weapon_
-    from creatures.attributes.status_ailment import \
+    from src.creatures.attributes.ability import Ability as Ability_
+    from src.creatures.attributes.element import Element
+    from src.creatures.attributes.item import Item as Item_
+    from src.creatures.attributes.move import Move as Move_
+    from src.creatures.attributes.reinforcements import Armor as Armor_
+    from src.creatures.attributes.reinforcements import Weapon as Weapon_
+    from src.creatures.attributes.status_ailment import \
         StatusAilment as StatusAilment_
-    from creatures.attributes.trait import Trait as Trait_
-    from creatures.humanoid import Humanoid as Humanoid_
-    from creatures.pymon import Pymon as Pymon_
-    from window.coord import Coord
-    from window.size import Size
-    
+    from src.creatures.attributes.trait import Trait as Trait_
+    from src.creatures.humanoid import Humanoid as Humanoid_
+    from src.creatures.pymon import Pymon as Pymon_
+    from src.window.color import Color as Color_
+    from src.window.coord import Coord
+    from src.window.screen import AnsiEscapeCode as AnsiEscapeCode_
+    from src.window.size import Size
+
 
 Creature = Union["Pymon_", "Humanoid_"]
 Attacker = Creature
 Defender = Creature
 User = Creature
+
+Color: TypeAlias = "Color_"
+AnsiEscapeCode: TypeAlias = "AnsiEscapeCode_"
+AnsiEscapeCodeOrColor = Union[Color, AnsiEscapeCode]  # TODO: Please rename
 
 Armor: TypeAlias = "Armor_"
 Weapon: TypeAlias = "Weapon_"
