61bd7f754972e2783aa2775f1c16ce1a8ff66b03
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2022-12-11 09:37:37 -0600

    Refactoring and Detailed Docstring

--- src/window/screen.py
+++ src/window/screen.py
@@ -1,11 +1,13 @@
-from typing import Optional, Tuple
-from src.types_ import CoordReference, Color
-from src.window.coord import Coord
-from os import system, name as os_name
 from enum import Enum
+from os import name as os_name
+from os import system
+from typing import Optional, Tuple
+
+from src.types_ import Color, CoordReference
+from src.window.coord import Coord
 
 
-class EscapeCode(Enum):
+class EscSeq(Enum):
     CURSOR_MOVE_UP = 0
     CURSOR_MOVE_DOWN = 1
     CURSOR_MOVE_LEFT = 2
@@ -14,59 +16,62 @@ class EscapeCode(Enum):
 
 
 class EscConverter:
-    """EscConverter class which acts as a mediator(logic separator)... # TODO: finish the doc
+    """EscConverter class which acts as a mediator (logic separator)."""
 
-    :param code: Type of an escape code
-    :param args: Sequence of escape sequence parameters
-    """
-    def __init__(self, code: EscapeCode | str, args: Tuple[str | int]) -> None:
+    def __init__(self, code: EscSeq | str, args: Tuple[str | int]) -> None:
         self.__code = code
         self.__args = args
 
     @property
-    def escape_code(self) -> str | None:
-        """Returns the escape code based on the escape code type. If string, returns it
-
-        :rtype: str | None
-        """
+    def escape_code(self) -> str:
+        """Returns the escape code based on the escape code type. If string, returns it."""
         if isinstance(self.__code, str):
             return self.__code
 
         match self.__code:
-            case EscapeCode.CURSOR_MOVE_UP:
+            case EscSeq.CURSOR_MOVE_UP:
                 return 'A'
-            case EscapeCode.CURSOR_MOVE_DOWN:
+            case EscSeq.CURSOR_MOVE_DOWN:
                 return 'B'
-            case EscapeCode.CURSOR_MOVE_LEFT:
+            case EscSeq.CURSOR_MOVE_LEFT:
                 return 'C'
-            case EscapeCode.CURSOR_MOVE_RIGHT:
+            case EscSeq.CURSOR_MOVE_RIGHT:
                 return 'D'
-            case EscapeCode.MOVE_CURSOR:
+            case EscSeq.MOVE_CURSOR:
                 return 'H'
             case _:
-                return None
+                return self.__code
 
     @property
     def escape_args(self) -> str:
         """Returns the args of the escape sequence.
-
-        :example: args = [12, 43] => '12;43'
-        :rtype: str
+        >>> [12, 43] -> '12;43'
         """
         return "".join([str(arg) + ';' for arg in self.__args])[:-1]
 
 
-class AnsiEscapeCode:
-    # TODO: Add the actual explanation to the class, try to follow: https://sphinx-rtd-tutorial.readthedocs.io/en/latest/docstrings.html
-    """https://en.wikipedia.org/wiki/ANSI_escape_code"""
-    CSI = '\033['
+class AnsiEscSeq:
+    """ANSI escape sequences are a standard for in-band signaling to control cursor location, color, font styling, and other options on terminals. 
+    
+    All ANSI escape sequences have a prefix; ESC (Escape).
+    - Octal: \\033
+    - Unicode: \\u001b
+    - Hexadecimal: \\x1B (\\x1b)
+    - Decimal: 27
+    
+    The most useful sequences start with CSI (Control Sequence Introducer)
+    - ESC[
+        
+    For example to make the color red: ESC[31m or \\033[31m
+    
+    Learn more here: https://en.wikipedia.org/wiki/ANSI_escape_code"""
+    ESC = '\033'
+    CSI = ESC + '['
 
-    def __init__(self, code: EscapeCode | str, *args: str | int) -> None:
-        print(code, args)
+    def __init__(self, code: EscSeq | str, *args: str | int) -> None:
         converter = EscConverter(code, args)
 
         self.code = self.CSI + converter.escape_args + converter.escape_code
-        # self.real = '\\' + self.code
 
     def execute(self) -> None:
         """Execute the ANSI escape code."""
@@ -74,37 +79,39 @@ class AnsiEscapeCode:
 
 
 class Cursor:
-    """https://en.wikipedia.org/wiki/ANSI_escape_code"""
+    """Keeps track of the cursor pos."""
     pos = Coord()
 
     @classmethod
-    def up(cls, n: int = 1) -> AnsiEscapeCode:
+    def up(cls, n: int = 1) -> AnsiEscSeq:
         """Moves the cursor `n` cells up."""
         cls.pos.y -= 1
-        return AnsiEscapeCode(EscapeCode.CURSOR_MOVE_UP, n - 1)
+        return AnsiEscSeq(EscSeq.CURSOR_MOVE_UP, n - 1)
 
     @classmethod
-    def down(cls, n: int = 1) -> AnsiEscapeCode:
+    def down(cls, n: int = 1) -> AnsiEscSeq:
         """Moves the cursor `n` cells down."""
         cls.pos.y += 1
-        return AnsiEscapeCode(EscapeCode.CURSOR_MOVE_DOWN, n - 1)
+        return AnsiEscSeq(EscSeq.CURSOR_MOVE_DOWN, n - 1)
 
     @classmethod
-    def right(cls, n: int = 1) -> AnsiEscapeCode:
+    def right(cls, n: int = 1) -> AnsiEscSeq:
         """Moves the cursor `n` cells right."""
         cls.pos.x += 1
-        return AnsiEscapeCode(EscapeCode.CURSOR_MOVE_RIGHT, n - 1)
+        return AnsiEscSeq(EscSeq.CURSOR_MOVE_RIGHT, n - 1)
 
     @classmethod
-    def left(cls, n: int = 1) -> AnsiEscapeCode:
+    def left(cls, n: int = 1) -> AnsiEscSeq:
         """Moves the cursor `n` cells left."""
         cls.pos.x -= 1
-        return AnsiEscapeCode(EscapeCode.CURSOR_MOVE_LEFT, n - 1)
+        return AnsiEscSeq(EscSeq.CURSOR_MOVE_LEFT, n - 1)
 
     @classmethod
-    def move(cls, pos: Coord) -> AnsiEscapeCode:
+    def move(cls, pos: CoordReference) -> AnsiEscSeq:
+        """Moves the cursor to the given pos."""
+        pos = Coord.convert_reference(pos)
         cls.pos = pos
-        return AnsiEscapeCode(EscapeCode.MOVE_CURSOR, *pos)
+        return AnsiEscSeq(EscSeq.MOVE_CURSOR, *pos)
 
 
 class Screen:
@@ -115,6 +122,7 @@ class Screen:
         color: Optional[Color] = None,
         move_cursor: bool = True
     ) -> None:
+        """Print text to the screen."""
         pos = Coord.convert_reference(pos)
 
         txt = str(txt)
@@ -124,9 +132,8 @@ class Screen:
 
         if color is not None:
             print(color, end='')
-            
+
         for line in txt.splitlines():
-            Cursor.left(Cursor.pos.x).execute()  # May need to change to pos.x instead of Cursor.pos.x
             print(line)
 
         Cursor.pos.x += txt.count('\n')
@@ -134,8 +141,13 @@ class Screen:
 
         if not move_cursor:
             Cursor.up(txt.count('\n') + 1).execute()
-            
+
     @staticmethod
     def clear():
         """Clear the screen. Works on all operating systems."""
         system('cls' if os_name == 'nt' else 'clear')
+
+    @staticmethod
+    def change_scene(scene: str):
+        Screen.clear()
+        Screen.write(scene)
