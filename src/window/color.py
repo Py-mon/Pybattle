ee931d22c453400af848dec83a862aaff9b203fb
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2022-12-11 09:36:07 -0600

    Refactoring

--- src/window/color.py
+++ src/window/color.py
@@ -1,11 +1,13 @@
 from typing import Optional
+
 from colorama import Fore
-from src.window.screen import AnsiEscapeCode
+
+from src.window.screen import AnsiEscSeq
 
 
 class Color:
     """A 4-bit color ANSI escape code."""
-    def __init__(self, color_code: AnsiEscapeCode, name: Optional[str] = None) -> None:
+    def __init__(self, color_code: AnsiEscSeq, name: Optional[str] = None) -> None:
         self._color_code = color_code.code
         if name is None:
             self.name = 'N/A'
@@ -19,19 +21,19 @@ class Color:
         return self._color_code
 
 class Colors:
-    DEFAULT = Color(AnsiEscapeCode(Fore.RESET), 'DEFAULT')
-    BLACK = Color(AnsiEscapeCode(Fore.BLACK), 'BLACK')                              # 0x000000
-    GRAY = Color(AnsiEscapeCode(Fore.LIGHTBLACK_EX), 'GRAY')                        # 0x666666
-    BRIGHT_WHITE = Color(AnsiEscapeCode(Fore.LIGHTWHITE_EX), 'BRIGHT_WHITE')        # 0xE5E5E5
-    BRIGHT_RED = Color(AnsiEscapeCode(Fore.LIGHTRED_EX), 'BRIGHT_RED')              # 0xF14C4C
-    RED = Color(AnsiEscapeCode(Fore.RED), 'RED')                                    # 0xCD3131
-    YELLOW = Color(AnsiEscapeCode(Fore.YELLOW), 'YELLOW')                           # 0xE5E510
-    BRIGHT_YELLOW = Color(AnsiEscapeCode(Fore.LIGHTYELLOW_EX), 'BRIGHT_YELLOW')     # 0xF5F543
-    BRIGHT_GREEN = Color(AnsiEscapeCode(Fore.LIGHTGREEN_EX), 'BRIGHT_GREEN')        # 0x23D18B
-    GREEN = Color(AnsiEscapeCode(Fore.GREEN), 'GREEN')                              # 0x0DBC79
-    CYAN = Color(AnsiEscapeCode(Fore.CYAN), 'CYAN')                                 # 0x11A8CD
-    BRIGHT_CYAN = Color(AnsiEscapeCode(Fore.LIGHTCYAN_EX), 'BRIGHT_CYAN')           # 0x29B8DB
-    BRIGHT_BLUE = Color(AnsiEscapeCode(Fore.LIGHTBLUE_EX), 'BRIGHT_BLUE')           # 0x3B8EEA
-    BLUE = Color(AnsiEscapeCode(Fore.BLUE), 'BLUE')                                 # 0x2472C8
-    MAGENTA = Color(AnsiEscapeCode(Fore.MAGENTA), 'MAGENTA')                        # 0xBC3FBC
-    BRIGHT_MAGENTA = Color(AnsiEscapeCode(Fore.LIGHTMAGENTA_EX), 'BRIGHT_MAGENTA')  # 0xD670D6
+    DEFAULT = Color(AnsiEscSeq(Fore.RESET), 'DEFAULT')
+    BLACK = Color(AnsiEscSeq(Fore.BLACK), 'BLACK')                              # 0x000000
+    GRAY = Color(AnsiEscSeq(Fore.LIGHTBLACK_EX), 'GRAY')                        # 0x666666
+    BRIGHT_WHITE = Color(AnsiEscSeq(Fore.LIGHTWHITE_EX), 'BRIGHT_WHITE')        # 0xE5E5E5
+    BRIGHT_RED = Color(AnsiEscSeq(Fore.LIGHTRED_EX), 'BRIGHT_RED')              # 0xF14C4C
+    RED = Color(AnsiEscSeq(Fore.RED), 'RED')                                    # 0xCD3131
+    YELLOW = Color(AnsiEscSeq(Fore.YELLOW), 'YELLOW')                           # 0xE5E510
+    BRIGHT_YELLOW = Color(AnsiEscSeq(Fore.LIGHTYELLOW_EX), 'BRIGHT_YELLOW')     # 0xF5F543
+    BRIGHT_GREEN = Color(AnsiEscSeq(Fore.LIGHTGREEN_EX), 'BRIGHT_GREEN')        # 0x23D18B
+    GREEN = Color(AnsiEscSeq(Fore.GREEN), 'GREEN')                              # 0x0DBC79
+    CYAN = Color(AnsiEscSeq(Fore.CYAN), 'CYAN')                                 # 0x11A8CD
+    BRIGHT_CYAN = Color(AnsiEscSeq(Fore.LIGHTCYAN_EX), 'BRIGHT_CYAN')           # 0x29B8DB
+    BRIGHT_BLUE = Color(AnsiEscSeq(Fore.LIGHTBLUE_EX), 'BRIGHT_BLUE')           # 0x3B8EEA
+    BLUE = Color(AnsiEscSeq(Fore.BLUE), 'BLUE')                                 # 0x2472C8
+    MAGENTA = Color(AnsiEscSeq(Fore.MAGENTA), 'MAGENTA')                        # 0xBC3FBC
+    BRIGHT_MAGENTA = Color(AnsiEscSeq(Fore.LIGHTMAGENTA_EX), 'BRIGHT_MAGENTA')  # 0xD670D6
