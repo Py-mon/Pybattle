3cf66638bbf21f153b9ba036e08a7e857a30697c
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2022-12-11 14:29:11 -0600

    Simplified Types

--- src/types_.py
+++ src/types_.py
@@ -1,42 +1,18 @@
-"""All the Types."""
-
-from typing import TYPE_CHECKING, TypeAlias, Union
+from typing import TYPE_CHECKING, Union, TypeAlias
 
 if TYPE_CHECKING:
-    from src.creatures.attributes.ability import Ability as Ability_
     from src.creatures.attributes.element import Element
-    from src.creatures.attributes.item import Item as Item_
-    from src.creatures.attributes.move import Move as Move_
-    from src.creatures.attributes.reinforcements import Armor as Armor_
-    from src.creatures.attributes.reinforcements import Weapon as Weapon_
-    from src.creatures.attributes.status_ailment import \
-        StatusAilment as StatusAilment_
-    from src.creatures.attributes.trait import Trait as Trait_
-    from src.creatures.humanoid import Humanoid as Humanoid_
-    from src.creatures.pymon import Pymon as Pymon_
-    from src.window.color import Color as Color_
+    from src.creatures.humanoid import Humanoid
+    from src.creatures.pymon import Pymon
     from src.window.coord import Coord
     from src.window.size import Size
 
 
-Creature = Union["Pymon_", "Humanoid_"]
+Creature = Union["Pymon", "Humanoid"]
 Attacker = Creature
 Defender = Creature
-User = Creature
+User: TypeAlias = "Humanoid"
 
-Color: TypeAlias = "Color_"
-
-Armor: TypeAlias = "Armor_"
-Weapon: TypeAlias = "Weapon_"
-Ability: TypeAlias = "Ability_"
-Item: TypeAlias = "Item_"
-Move: TypeAlias = "Move_"
-StatusAilment: TypeAlias = "StatusAilment_"
-Trait: TypeAlias = "Trait_"
-Humanoid: TypeAlias = "Humanoid_"
-
-ElementReference: TypeAlias = Union[str, "Element"]
-
-CoordReference: TypeAlias = Union["Coord", tuple, int]
-
-SizeReference: TypeAlias = Union["Size", tuple, int]
+ElementReference = Union[str, "Element"]
+CoordReference = Union["Coord", tuple, int]
+SizeReference = Union["Size", tuple, int]
