8ae8e097fde66118e3efcba72e69d1d10c811ebc
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2022-12-17 20:24:34 -0600

    New Window (Scene) System

--- main.py
+++ main.py
@@ -2,21 +2,23 @@ from pybattle.ansi.screen import Screen
 from pybattle.window.frame import Frame
 from pybattle.ansi.color import Colors
 from pybattle.window.matrix import Matrix, ColorCoord
+from pybattle.window.window import Window
+
 Screen.clear()
 
 # text = '''
 # KITTY CATS
 # EAT CHICKENS'''
   
-# window = Frame(text, (6, 15))
-# window.add_frame(Frame(size=(2, 5)))
+# window0 = Frame(text, (6, 15))
+# window0.add_frame(Frame(size=(2, 5)))
 
+window1 = Window(Frame(size=(5, 5)))
+window2 = Window(Frame(size=(4, 4)))
+window3 = Window(Frame(size=(6, 6)))
 
-from pybattle.ansi.input import SelectionMenu, Selection
+window1.set()
+window2.set()
+window3.set()
 
-
-SelectionMenu((15, 15), [Selection('a', (1, 1)), Selection('b', (4, 4)), Selection('c', (8, 8))])
-# x = Matrix('hello', ColorCoord(0, Colors.GRAY))
-# print(x)
-# y = Frame(x)
-# print(y.matrix)
+Window.show()
---
+++ pybattle/window/window.py
@@ -0,0 +1,34 @@
+from pybattle.ansi.screen import Screen
+from pybattle.window.frame import Frame
+from typing import List
+
+
+class Window:
+    windows: List[Frame] = []
+    window = None
+
+    def __init__(self, frame: Frame) -> None:
+        self.__window = frame
+
+    @classmethod
+    def show(cls, index: int = 0) -> None:
+        """Show the current Window.
+        
+        index: 
+        ```
+        0 -> first one
+        -1 -> previous one
+        ...
+        """
+        Screen.clear()
+        if cls.window is not None:
+            Screen.write(cls.windows[index - 1])
+        
+    def set(self) -> None:
+        """Set the window."""
+        Window.window = self.__window
+        Window.windows.append(Window.window)
+
+    def set_and_show(self) -> None:
+        self.set()
+        Window.show()
