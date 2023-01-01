f93c75eecc7ad9d46958b15cd27e05625840b8cb
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-01-01 14:47:39 -0600

    Fixed and Added SelectionRange

--- pybattle/window/range.py
+++ pybattle/window/range.py
@@ -4,29 +4,48 @@ from pybattle.types_ import CoordReference
 from pybattle.window.coord import Coord
 
 
-class Range:
+class RectRange:
     def __init__(
-        self, stop: CoordReference,
+        self, 
+        stop: CoordReference,
         start: Optional[CoordReference] = None
     ) -> None:
-        self.__start = Coord(start or 0)
-        self.__stop = Coord(stop)
-        
-        if self.__start.y == self.__stop.y:
-            self.__stop.y += 1
-        if self.__start.x == self.__stop.x:
-            self.__stop.x += 1
-        
+        self._start = Coord(start or 0)
+        self._stop = Coord(stop)
+
     def __iter__(self) -> Generator[Coord, None, None]:
-        for row in self.rows_coords:
-            for coord in row:
-                yield coord
-                
+        lst = [coord for row in self.array_coords for coord in row]
+        yield from lst
+
     def __contains__(self, coord: CoordReference) -> bool:
         return Coord(coord) in iter(self)
 
     @property
-    def rows_coords(self) -> list[list[Coord]]:
+    def array_coords(self) -> list[list[Coord]]:
         return [[
-            Coord(y, x) for x in range(self.__start.x, self.__stop.x)]
-            for y in range(self.__start.y, self.__stop.y)]  # Strange + 1
+            Coord(y, x) for x in range(self._start.x, self._stop.x + 1)]
+            for y in range(self._start.y, self._stop.y + 1)]
+    
+    def __repr__(self) -> str:
+        return str(self.array_coords)
+
+
+
+class SelectionRange(RectRange):
+    def __init__(
+        self, 
+        width: int,
+        stop: CoordReference,
+        start: Optional[CoordReference] = None,        
+    ) -> None:
+        super().__init__(stop, start)
+        self.width = width
+        
+    @property
+    def array_coords(self) -> list[Coord]:
+        res = [[
+            Coord(y, x) for x in range(self.width)]
+            for y in range(self._start.y, self._stop.y + 1)]
+        res[0] = res[0][self._start.x:]
+        res[-1] = res[-1][:self._stop.x + 1]
+        return res
