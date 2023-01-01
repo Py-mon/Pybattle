62942a15fbffafd99f1b7064c56fa2978c1fb04f
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-01-01 14:49:55 -0600

    Edited Tests

--- tests/bedroom.py
+++ tests/bedroom.py
@@ -1,5 +1,9 @@
 from pybattle.window.frames.map_frame import MapFrame
 from pybattle.window.frames.frame import Frame
+from pybattle.window.frames.center_frame import CenteredFrame
+from pybattle.ansi.colors import Color
+from pybattle.window.matrix import Matrix
+
 
 
 map_ = MapFrame('''
@@ -13,6 +17,8 @@ map_ = MapFrame('''
 ╰│╯   ╶─╯           ╭─────┬─╮ 
                     │░░░░░│▓│ 
                     ╰─────┴─╯ 
-''', title='BEDROOM')
+''', 'BEDROOM', Color.RED, Color.BLUE)
 
 map_.add_frame(Frame((2, 9)), (0, 4))
+
+print(map_)
