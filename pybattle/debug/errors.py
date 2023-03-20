e1125522e8813a4a240615bc7b1d9960f4ef988d
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-03-19 20:20:36 -0500

    Errors derive from similar built-in errors

--- pybattle/debug/errors.py
+++ pybattle/debug/errors.py
@@ -1,32 +1,18 @@
-from pybattle.debug.traceback import Traceback
-
-
 class Error(Exception):
     """The base traceback error."""
-    def __init__(self, msg, traceback: bool = True) -> None:
-        self.msg = msg
-        
-        if traceback:
-            self.traceback = Traceback()
-        else:
-            self.traceback = None
-
-    def __str__(self) -> str:
-        if self.traceback is None:
-            return self.msg
-        return self.traceback.trace + ' ' + self.msg
+    pass
 
 
-class OutOfBoundsError(Error):
+class OutOfBoundsError(Error, IndexError):
     """Coords out of bounds."""
     pass
 
 
-class InsufficientArgumentsError(Error):
+class InsufficientArgumentsError(Error, TypeError):
     """Not enough arguments required."""
     pass
 
 
-class InvalidMethodError(Error):
+class InvalidMethodError(Error, NameError):
     """Invalid method."""
     pass
