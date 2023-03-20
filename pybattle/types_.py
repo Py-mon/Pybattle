3f988ae5fa9799df8f0164fc2897a06eb0826239
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-03-19 19:57:47 -0500

    New Types

--- pybattle/types_.py
+++ pybattle/types_.py
@@ -1,12 +1,14 @@
+from enum import Enum
 from typing import TYPE_CHECKING, TypeAlias, Union
 
 if TYPE_CHECKING:
+    from pybattle.ansi.colors import ColorType
     from pybattle.creatures.attributes.element import Element
     from pybattle.creatures.humanoid import Humanoid
     from pybattle.creatures.pymon import Pymon
     from pybattle.window.grid.coord import Coord
     from pybattle.window.grid.size import Size
-    from pybattle.window.grid.matrix import Matrix
+    from pybattle.window.grid.range import RectRange, SelectionRange
 
 
 Creature = Union["Pymon", "Humanoid"]
@@ -17,6 +19,31 @@ User: TypeAlias = "Humanoid"
 ElementReference = Union[str, "Element"]
 CoordReference = Union["Coord", tuple, int]
 SizeReference = Union["Size", CoordReference]
-MatrixReference = Union["Matrix", str, list, SizeReference, list, list[list]]
 
-Reference2D = tuple | int
+ColorRange = tuple["ColorType", Union["RectRange", "SelectionRange"]]
+
+
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+
+class Align(Enum):
+    LEFT = 0
+    RIGHT = 1
+    CENTER = 2
+    MIDDLE = 2
+
+
+class Thickness(Enum):
+    THIN = 0
+    THICK = 1
+    DOUBLE = 2
+
+
+Junction = dict[Direction, Thickness]
