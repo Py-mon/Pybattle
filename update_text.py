725f48732fd8fcc37b56b7f17b382cb3ea3e3d26
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-09-27 18:48:26 -0500

    import

--- update_text.py
+++ update_text.py
@@ -6,7 +6,8 @@
 # frame = Frame(Cell.from_size(Size(2, 5)))
 # with open("txt.txt", "w", encoding='utf-8') as f:
 #     f.write(str(frame))
-
+import sys
+sys.path.append(r'C:\Users\jacob\Downloads\Programming\Python\Pybattle\Github')
 from pybattle.screen.frames.frame import Frame
 
 from pybattle.screen.grid.point import Coord, Size
