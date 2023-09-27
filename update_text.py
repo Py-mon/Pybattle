67e503564131a16eec05ca543a8dc3e5628654f1
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-09-27 18:26:56 -0500

    worky

--- update_text.py
+++ update_text.py
@@ -1,9 +1,17 @@
+# from pybattle.screen.frames.frame import Frame
+
+# from pybattle.screen.grid.point import Coord, Size
+# from pybattle.screen.grid.cell import Cell
+
+# frame = Frame(Cell.from_size(Size(2, 5)))
+# with open("txt.txt", "w", encoding='utf-8') as f:
+#     f.write(str(frame))
+
 from pybattle.screen.frames.frame import Frame
 
 from pybattle.screen.grid.point import Coord, Size
 from pybattle.screen.grid.cell import Cell
 
 frame = Frame(Cell.from_size(Size(2, 5)))
-with open("txt.txt", "w", encoding='utf-8') as f:
-    f.write(str(frame))
 
+pyscript.write("text", str(frame))
