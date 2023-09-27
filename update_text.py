c4aa15bc32c41bcb6ac434f25bdca470133ceafa
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-09-27 18:53:06 -0500

    might

--- update_text.py
+++ update_text.py
@@ -7,13 +7,11 @@
 # with open("txt.txt", "w", encoding='utf-8') as f:
 #     f.write(str(frame))
 import sys
-sys.path.append(r'C:\Users\jacob\Downloads\Programming\Python\Pybattle\Github')
+
+sys.path.append(r"C:\Users\jacob\Downloads\Programming\Python\Pybattle\Github")
 import x
-from pybattle.screen.frames.frame import Frame
 
-from pybattle.screen.grid.point import Coord, Size
-from pybattle.screen.grid.cell import Cell
 
-frame = Frame(Cell.from_size(Size(2, 5)))
+frame = x.Frame(x.Cell.from_size(x.Size(2, 5)))
 
 pyscript.write("text", str(frame))
---
+++ x.py
@@ -0,0 +1,4 @@
+from pybattle.screen.frames.frame import Frame
+
+from pybattle.screen.grid.point import Coord, Size
+from pybattle.screen.grid.cell import Cell
