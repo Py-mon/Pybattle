c13ff86d3888e5ee34edd12c83e0d3b73f942a81
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-09-27 19:11:59 -0500

    toml

--- update_text.py
+++ update_text.py
@@ -6,12 +6,13 @@
 # frame = Frame(Cell.from_size(Size(2, 5)))
 # with open("txt.txt", "w", encoding='utf-8') as f:
 #     f.write(str(frame))
-import sys
 
-sys.path.append(r"C:\Users\jacob\Downloads\Programming\Python\Pybattle\Github")
-import x
+from pybattle.screen.frames.frame import Frame
+
+from pybattle.screen.grid.point import Coord, Size
+from pybattle.screen.grid.cell import Cell
 
 
-frame = x.Frame(x.Cell.from_size(x.Size(2, 5)))
-
+frame = Frame(Cell.from_size(Size(2, 5)))
+ 
 pyscript.write("text", str(frame))
--- x.py
+++
@@ -1,4 +0,0 @@
-from pybattle.screen.frames.frame import Frame
-
-from pybattle.screen.grid.point import Coord, Size
-from pybattle.screen.grid.cell import Cell
