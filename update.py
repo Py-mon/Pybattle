39262c2d8516a6b37f8d825c4212b375a4798d31
PythonDominator <102424561+PythonDominator@users.noreply.github.com>
2023-10-01 10:54:06 -0500

    mobile

--- update.py
+++ update.py
@@ -1,5 +1,5 @@
 from create import update, update_player, update_weather
-from movement import key_up, key_down
+from movement import key_up, key_down, keys_pressing
 import asyncio
 from element import Element, document
 from pyodide.ffi import create_proxy
@@ -17,3 +17,27 @@ document.onkeydown = create_proxy(key_down)
 document.onkeyup = create_proxy(key_up)
 
 asyncio.ensure_future(myLongFunction())
+
+
+def get_dir_func(c):
+    def y(_):
+        async def x():
+            keys_pressing.add(c)
+            await asyncio.sleep(0.05)
+            keys_pressing.remove(c)
+
+        asyncio.ensure_future(x())
+
+    return create_proxy(y)
+
+
+Element("up").add_event("click", get_dir_func("w"))
+
+
+Element("left").add_event("click", get_dir_func("a"))
+
+
+Element("down").add_event("click", get_dir_func("s"))
+
+
+Element("right").add_event("click", get_dir_func("d"))
